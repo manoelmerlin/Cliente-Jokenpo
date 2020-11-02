5b450888bc29cafb84bf8aa02d49341b68d473ae
manoelmerlin <manoelmerlin@hotmail.com>
2020-11-01 21:55:46 -0300

    Correções cliente

--- src/main/java/jokenpocliente/Cliente.java
+++ src/main/java/jokenpocliente/Cliente.java
@@ -11,19 +11,33 @@ public class Cliente {
 
         try {
             String msgDoServer;
+            String msgDoCliente;
+            Boolean pvp = false;
             socket = new Socket("localhost", 8081);
+            System.out.println("Bem vindo para sair a qualquer momento digite 10");
             Comunicacao comunicacao = new Comunicacao(socket);
             msgDoServer = comunicacao.ReceberMsg();
+    
             System.out.println(msgDoServer);
-            String msgDoCliente;
             while (true) {
                 msgDoCliente = entrada.nextLine();
-                comunicacao.EnviarMsg(msgDoCliente);           
-                System.out.println("Aguarde..");
+                comunicacao.EnviarMsg(msgDoCliente);
+
+                if (msgDoCliente.equals("10")) {
+                    System.out.println("Saindo.....");
+                    socket.close();
+                }
+
+                if (pvp) {
+                    System.out.println("Aguarde o outro jogador");
+                } else {
+                    System.out.println("Aguarde..");
+                }     
+
                 System.out.println(comunicacao.ReceberMsg());
             }
         } catch (Exception e) {
-            System.out.println("Nao consegui resolver o host...");
+            System.out.println("Desconectado do jogo...");
         }
     }
 }
